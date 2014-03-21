d70045f38c0bec37afb9a07415843eab9f742ffe
Ohondulus <ohondulus@gmail.com>
2014-03-21 15:28:05 +0100

    Create Logger.java

---
+++ Logger.java
@@ -0,0 +1,26 @@
+public class Logger {
+	static int depths;
+	
+	public Logger(){
+		depths = 0;
+	}
+	
+	public static void Log(int direction, String text){
+		if(direction==1){
+			for(int i=0; i<depths; i++){
+				System.out.print("\t");
+			}
+			depths = depths + direction;
+			System.out.print("->");
+			System.out.print(depths+". "+text+"\n");
+		}
+		else{
+			for(int i=0; i<depths-1; i++){
+				System.out.print("\t");
+			}
+			System.out.print("<-");
+			System.out.print(depths+". "+text+"\n");
+			depths = depths + direction;
+		}
+	}
+}
