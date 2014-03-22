9fbb6275b601b7c747c63312865d044d2d6f1758
Gabor Nagy <anon>
2014-03-22 12:32:39 +0100

    Correct indents and remove accentuated letters

--- Program.java
+++ Program.java
@@ -5,263 +5,260 @@ public class Program {
 
 	public static void main(String[] args){
 		
-		//Konzolról való beolvasás
+		//Konzolrol valo beolvasas
 		Scanner scanner = new Scanner (System.in);
 		
 		while (true) {
-				//Use-case menü kiíratása
-		       	System.out.println("[1]  Ellenseg letrehozasa"); 
-		        System.out.println("[2]  Ellenseg celba er"); 
-		        System.out.println("[3]  Ellenseg talalkozik egy masik ellenseggel");
-		        System.out.println("[4]  Ellenseg akadalyra lep");
-		        System.out.println("[5]  Torony lerakas");
-		        System.out.println("[6]  Akadaly lerakasa");
-		        System.out.println("[7]  Varazsko letrehozasa");
-		        System.out.println("[8]  Torony fejlesztes");
-		        System.out.println("[9]  Akadaly fejlesztes");
-		        System.out.println("[10] Torony lo");
-		        System.out.println("[11] Kilepes");
+			//Use-case menu kiiratasa
+			System.out.println("[1]  Ellenseg letrehozasa");
+			System.out.println("[2]  Ellenseg celba er");
+			System.out.println("[3]  Ellenseg talalkozik egy masik ellenseggel");
+			System.out.println("[4]  Ellenseg akadalyra lep");
+			System.out.println("[5]  Torony lerakas");
+			System.out.println("[6]  Akadaly lerakasa");
+			System.out.println("[7]  Varazsko letrehozasa");
+			System.out.println("[8]  Torony fejlesztes");
+			System.out.println("[9]  Akadaly fejlesztes");
+			System.out.println("[10] Torony lo");
+			System.out.println("[11] Kilepes");
 
+			System.out.print("Melyik Use-Case-t szeretne? : ");
 
-		        System.out.print("Melyik Use-Case-t szeretne? : "); 
-		        
-		        //Megvizsgálja, hogy a felhasználó nem számtól eltérõ karaktert adott meg
-		        while (!scanner.hasNextInt()) {
-		        	System.out.println("Nem lehet karaktert megadni!!!");
-		        	System.out.print("Adjon meg egy ervenyes szamot: "); 
-		        	
-		        	scanner.next();
-		        }
+			//Megvizsgalja, hogy a felhasznalo nem szamtol eltero karaktert adott meg
+			while (!scanner.hasNextInt()) {
+				System.out.println("Nem lehet karaktert megadni!!!");
+				System.out.print("Adjon meg egy ervenyes szamot: "); 
 
-		        int selection = scanner.nextInt();
+				scanner.next();
+			}
 
-		        //Kilépési pont biztosítása
-		        if (selection == 11) {
-		                break;
-		        }
+			int selection = scanner.nextInt();
 
-		        //Az egyes számok esetén meghívódó esemény lánculatok
-		        switch (selection){
-		             
-		           case 1:
-		        	for(int i=0; i<5; i++){System.out.println();}
-		        	System.out.println("****************************");   
-		        	System.out.println("Ellenseg letrehozasa");
-		        	
-		        	/*
-		        	 * Ide jön a use-case
-		        	 */
-		        	
-		        	//Ha lefutott a use-case vár az enterre a felhasználótól
-		        	try{
-		        		System.out.println("Kerem nyomjon ENTERT!");
-		        		System.in.read();
-		        	}
-		        	catch (IOException e) {
-		                e.printStackTrace();
-		            }
-		        	
-		        	for(int i=0; i<5; i++){System.out.println();}
-		        	System.out.println("****************************");
-		            break;
-		            
-		           case 2:
-		        	   for(int i=0; i<5; i++){System.out.println();}
-			        	System.out.println("****************************");   
-			        	System.out.println("Ellenseg celba er");
-			        	
-			        	/*
-			        	 * Ide jön a use-case
-			        	 */
-			        	
-			        	try{
-			        		System.out.println("Kerem nyomjon ENTERT!");
-			        		System.in.read();
-			        	}
-			        	catch (IOException e) {
-			                e.printStackTrace();
-			            }
-			        	
-			        	for(int i=0; i<5; i++){System.out.println();}
-			        	System.out.println("****************************");
-		                break;
-		                
-		           case 3:
-		        	   for(int i=0; i<5; i++){System.out.println();}
-			        	System.out.println("****************************");   
-			        	System.out.println("Ellenseg talalkozik egy masik ellenseggel");
-			        	
-			        	/*
-			        	 * Ide jön a use-case
-			        	 */
-			        	
-			        	try{
-			        		System.out.println("Kerem nyomjon ENTERT!");
-			        		System.in.read();
-			        	}
-			        	catch (IOException e) {
-			                e.printStackTrace();
-			            }
-			        	
-			        	for(int i=0; i<5; i++){System.out.println();}
-			        	System.out.println("****************************");
-	                	break;
-	                	
-		           case 4:
-		        	   for(int i=0; i<5; i++){System.out.println();}
-			        	System.out.println("****************************");   
-			        	System.out.println("Ellenseg akadalyra lep");
-			        	
-			        	/*
-			        	 * Ide jön a use-case
-			        	 */
-			        	
-			        	try{
-			        		System.out.println("Kerem nyomjon ENTERT!");
-			        		System.in.read();
-			        	}
-			        	catch (IOException e) {
-			                e.printStackTrace();
-			            }
-			        	
-			        	for(int i=0; i<5; i++){System.out.println();}
-			        	System.out.println("****************************");
-	                	break;
-		           
-		           case 5:
-		        	   for(int i=0; i<5; i++){System.out.println();}
-			        	System.out.println("****************************");   
-			        	System.out.println("Torony lerakas");
-			        	
-			        	/*
-			        	 * Ide jön a use-case
-			        	 */
-			        	
-			        	try{
-			        		System.out.println("Kerem nyomjon ENTERT!");
-			        		System.in.read();
-			        	}
-			        	catch (IOException e) {
-			                e.printStackTrace();
-			            }
-			        	
-			        	for(int i=0; i<5; i++){System.out.println();}
-			        	System.out.println("****************************");
-	                	break;
-	                	
-		           case 6:
-		        	   for(int i=0; i<5; i++){System.out.println();}
-			        	System.out.println("****************************");   
-			        	System.out.println("Akadaly lerakasa");
-			        	
-			        	/*
-			        	 * Ide jön a use-case
-			        	 */
-			        	
-			        	try{
-			        		System.out.println("Kerem nyomjon ENTERT!");
-			        		System.in.read();
-			        	}
-			        	catch (IOException e) {
-			                e.printStackTrace();
-			            }
-			        	
-			        	for(int i=0; i<5; i++){System.out.println();}
-			        	System.out.println("****************************");
-	                	break;
-	                	
-		           case 7:
-		        	   for(int i=0; i<5; i++){System.out.println();}
-			        	System.out.println("****************************");   
-			        	System.out.println("Varazsko letrehozasa");
-			        	
-			        	/*
-			        	 * Ide jön a use-case
-			        	 */
-			        	
-			        	try{
-			        		System.out.println("Kerem nyomjon ENTERT!");
-			        		System.in.read();
-			        	}
-			        	catch (IOException e) {
-			                e.printStackTrace();
-			            }
-			        	
-			        	for(int i=0; i<5; i++){System.out.println();}
-			        	System.out.println("****************************");
-	                	break;
-	                	
-		           case 8:
-		        	   for(int i=0; i<5; i++){System.out.println();}
-			        	System.out.println("****************************");   
-			        	System.out.println("Torony fejlesztes");
-			        	
-			        	/*
-			        	 * Ide jön a use-case
-			        	 */
-			        	
-			        	try{
-			        		System.out.println("Kerem nyomjon ENTERT!");
-			        		System.in.read();
-			        	}
-			        	catch (IOException e) {
-			                e.printStackTrace();
-			            }
-			        	
-			        	for(int i=0; i<5; i++){System.out.println();}
-			        	System.out.println("****************************");
-	                	break;
-	               
-		           case 9:
-		        	   for(int i=0; i<5; i++){System.out.println();}
-			        	System.out.println("****************************");   
-			        	System.out.println("Akadaly fejlesztes");
-			        	
-			        	/*
-			        	 * Ide jön a use-case
-			        	 */
-			        	
-			        	try{
-			        		System.out.println("Kerem nyomjon ENTERT!");
-			        		System.in.read();
-			        	}
-			        	catch (IOException e) {
-			                e.printStackTrace();
-			            }
-			        	
-			        	for(int i=0; i<5; i++){System.out.println();}
-			        	System.out.println("****************************");
-	                	break;
-	                	
-		           case 10:
-		        	   for(int i=0; i<5; i++){System.out.println();}
-			        	System.out.println("****************************");   
-			        	System.out.println("Torony lo");
-			        	
-			        	/*
-			        	 * Ide jön a use-case
-			        	 */
-			        	
-			        	try {
-			        		System.out.println("Kerem nyomjon ENTERT!");
-			        		System.in.read();
-			        	}
-			        	catch (IOException e) {
-			                e.printStackTrace();
-			            }
-			        	
-			        	for(int i=0; i<5; i++){System.out.println();}
-			        	System.out.println("****************************");
-	                	break;
-	               
-	               //Ha a menüben jelzettõl eltérõ sorszámot adna meg a felhasználó 	
-		           default:
-		        	   for(int i=0; i<5; i++){System.out.println();}
-		        	   System.out.println("Rossz sorszamot adott meg!!");
-		        	   System.out.println("****************************");
-		       };
+			//Kilepesi pont biztositasa
+			if (selection == 11) {
+				break;
+			}
+
+			//Az egyes szamok eseten meghivodo esemeny lanculatok
+			switch (selection){
+
+				case 1:
+					for(int i=0; i<5; i++){System.out.println();}
+					System.out.println("****************************");   
+					System.out.println("Ellenseg letrehozasa");
+
+					/*
+					* Ide jon a use-case
+					*/
+
+					//Ha lefutott a use-case var az enterre a felhasznalotol
+					try{
+						System.out.println("Kerem nyomjon ENTERT!");
+						System.in.read();
+					}
+					catch (IOException e) {
+						e.printStackTrace();
+					}
+
+					for(int i=0; i<5; i++){System.out.println();}
+					System.out.println("****************************");
+					break;
+
+				case 2:
+					for(int i=0; i<5; i++){System.out.println();}
+					System.out.println("****************************");   
+					System.out.println("Ellenseg celba er");
+
+					/*
+					* Ide jon a use-case
+					*/
+
+					try{
+						System.out.println("Kerem nyomjon ENTERT!");
+						System.in.read();
+					}
+					catch (IOException e) {
+						e.printStackTrace();
+					}
+
+					for(int i=0; i<5; i++){System.out.println();}
+					System.out.println("****************************");
+					break;
+
+				case 3:
+					for(int i=0; i<5; i++){System.out.println();}
+					System.out.println("****************************");   
+					System.out.println("Ellenseg talalkozik egy masik ellenseggel");
+
+					/*
+					* Ide jon a use-case
+					*/
+
+					try{
+						System.out.println("Kerem nyomjon ENTERT!");
+						System.in.read();
+					}
+					catch (IOException e) {
+						e.printStackTrace();
+					}
+
+					for(int i=0; i<5; i++){System.out.println();}
+					System.out.println("****************************");
+					break;
+
+				case 4:
+					for(int i=0; i<5; i++){System.out.println();}
+					System.out.println("****************************");   
+					System.out.println("Ellenseg akadalyra lep");
+
+					/*
+					* Ide jon a use-case
+					*/
+
+					try{
+						System.out.println("Kerem nyomjon ENTERT!");
+						System.in.read();
+					}
+					catch (IOException e) {
+						e.printStackTrace();
+					}
+
+					for(int i=0; i<5; i++){System.out.println();}
+					System.out.println("****************************");
+					break;
+
+				case 5:
+					for(int i=0; i<5; i++){System.out.println();}
+					System.out.println("****************************");   
+					System.out.println("Torony lerakas");
+
+					/*
+					* Ide jon a use-case
+					*/
+
+					try{
+						System.out.println("Kerem nyomjon ENTERT!");
+						System.in.read();
+					}
+					catch (IOException e) {
+						e.printStackTrace();
+					}
+
+					for(int i=0; i<5; i++){System.out.println();}
+					System.out.println("****************************");
+					break;
+
+				case 6:
+					for(int i=0; i<5; i++){System.out.println();}
+					System.out.println("****************************");   
+					System.out.println("Akadaly lerakasa");
+
+					/*
+					* Ide jon a use-case
+					*/
+
+					try{
+						System.out.println("Kerem nyomjon ENTERT!");
+						System.in.read();
+					}
+					catch (IOException e) {
+						e.printStackTrace();
+					}
+
+					for(int i=0; i<5; i++){System.out.println();}
+					System.out.println("****************************");
+					break;
+
+				case 7:
+					for(int i=0; i<5; i++){System.out.println();}
+					System.out.println("****************************");   
+					System.out.println("Varazsko letrehozasa");
+
+					/*
+					* Ide jon a use-case
+					*/
+
+					try{
+						System.out.println("Kerem nyomjon ENTERT!");
+						System.in.read();
+					}
+					catch (IOException e) {
+						e.printStackTrace();
+					}
+
+					for(int i=0; i<5; i++){System.out.println();}
+					System.out.println("****************************");
+					break;
+
+				case 8:
+					for(int i=0; i<5; i++){System.out.println();}
+					System.out.println("****************************");   
+					System.out.println("Torony fejlesztes");
+
+					/*
+					* Ide jon a use-case
+					*/
+
+					try{
+						System.out.println("Kerem nyomjon ENTERT!");
+						System.in.read();
+					}
+					catch (IOException e) {
+						e.printStackTrace();
+					}
+
+					for(int i=0; i<5; i++){System.out.println();}
+					System.out.println("****************************");
+					break;
+
+				case 9:
+					for(int i=0; i<5; i++){System.out.println();}
+					System.out.println("****************************");   
+					System.out.println("Akadaly fejlesztes");
+
+					/*
+					* Ide jon a use-case
+					*/
+
+					try{
+						System.out.println("Kerem nyomjon ENTERT!");
+						System.in.read();
+					}
+					catch (IOException e) {
+						e.printStackTrace();
+					}
+
+					for(int i=0; i<5; i++){System.out.println();}
+					System.out.println("****************************");
+					break;
+
+				case 10:
+					for(int i=0; i<5; i++){System.out.println();}
+					System.out.println("****************************");   
+					System.out.println("Torony lo");
+
+					/*
+					* Ide jon a use-case
+					*/
+
+					try {
+						System.out.println("Kerem nyomjon ENTERT!");
+						System.in.read();
+					}
+					catch (IOException e) {
+						e.printStackTrace();
+					}
+
+					for(int i=0; i<5; i++){System.out.println();}
+					System.out.println("****************************");
+					break;
+
+				//Ha a menuben jelzettol eltero sorszamot adna meg a felhasznalo 	
+				default:
+					for(int i=0; i<5; i++){System.out.println();}
+					System.out.println("Rossz sorszamot adott meg!!");
+					System.out.println("****************************");
+			};
 		}
-
 	}
-
 }
